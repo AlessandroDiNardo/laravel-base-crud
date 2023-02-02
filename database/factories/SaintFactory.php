a1b0a95e42e92ac8346e5feb30ff97a3f8aae65d
AlessandroDiNardo <alexdna003@gmail.com>
2023-02-02 18:26:38 +0100

    create factory

---
+++ database/factories/SaintFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class SaintFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            "nome" => fake () -> name() ,
+            "luogo_di_nascita" => fake () -> city(),
+            "data_benedizione" => fake () -> dateTime(),
+            "numero_miracoli" => fake () -> numberBetween(1, 1000) ,
+        ];
+    }
+}
