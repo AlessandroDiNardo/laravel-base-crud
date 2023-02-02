7ab2b449e6776ad1ee81882c2a85620dd6de02d1
AlessandroDiNardo <alexdna003@gmail.com>
2023-02-02 18:56:36 +0100

    esercizio completato

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Saint;
+
 class MainController extends Controller
 {
+    
     public function home() {
 
-        return view('pages.home'); 
-    }
+        $saint = Saint::all();
+
+        $data = [
+
+            'saints' => $saint
+        ];
+
+        return view('pages.home' , $data);
+    } 
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,4 +2,15 @@
 
 @section('content')
     <h1>Santi</h1>
+
+    <ul>
+        @foreach ($saints as $saint)
+            <li>
+                <span>Nome: <h3> {{$saint -> nome }}</h3></span>
+                <span>Luogo Di Nascina: <h3> {{$saint -> luogo_di_nascita}}</h3></span>
+                <span>Data di Benedizione: <h3> {{$saint -> data_benedizione}}</h3></span>
+                <span>Numero di Miracoli: <h3> {{$saint -> numero_miracoli}}</h3></span>
+            </li>
+        @endforeach
+    </ul>
 @endsection
